34079401aa6a175bbc63fd90728eadb438171751
hezhenting <1018714318@qq.com>
2023-12-31 14:00:06 +0800

    Initial commit

---
+++ src/main/java/com/graduation/campustakeawayplatform/CampusTakeawayPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.campustakeawayplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusTakeawayPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusTakeawayPlatformApplication.class, args);
+    }
+
+}
