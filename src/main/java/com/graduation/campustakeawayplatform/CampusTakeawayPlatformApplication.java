687c28394dedb7014be82757615dd47f17d9e111
hezhenting <1018714318@qq.com>
2023-12-31 15:11:54 +0800

    基本目录整理

--- src/main/java/com/graduation/campustakeawayplatform/CampusTakeawayPlatformApplication.java
+++ src/main/java/com/graduation/campustakeawayplatform/CampusTakeawayPlatformApplication.java
@@ -1,9 +1,11 @@
 package com.graduation.campustakeawayplatform;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfiguration
 public class CampusTakeawayPlatformApplication {
 
     public static void main(String[] args) {
