c972524f5dd8f55ee4107b74360b04a11edfe245
hezhenting <1018714318@qq.com>
2023-12-31 20:14:12 +0800

    @{feat} #{creat} ${引用雪花id}

---
+++ src/main/java/com/graduation/campustakeawayplatform/common/hutool/IdGenerator.java
@@ -0,0 +1,25 @@
+package com.graduation.campustakeawayplatform.common.hutool;
+
+/**
+ * @author hzt
+ * @version 1.0
+ * @date 2023/12/31 20:00
+ */
+import cn.hutool.core.lang.Snowflake;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IdGenerator {
+
+    private final Snowflake snowflake;
+
+    public IdGenerator() {
+        // 参数1是机器ID，参数2是数据中心ID
+        this.snowflake = new Snowflake(1, 1);
+    }
+
+    public long generateId() {
+        return snowflake.nextId();
+    }
+}
+
